79179687d1a587c5ae6b40aeb6cf94d4e3125e36
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-09-01 23:31:44 -0500

    Use variables for ethminer command

--- ethermine.sh
+++ ethermine.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+ETHER_ADDR=0xDd9d279854C8D614d37a492192A40f678f476BC1
+SERVER1=us1.ethermine.org:4444
+SERVER2=eu1.ethermine.org:4444
+
 setx GPU_FORCE_64BIT_PTR        0
 setx GPU_MAX_HEAP_SIZE          100
 setx GPU_USE_SYNC_OBJECTS       1
 setx GPU_MAX_ALLOC_PERCENT      100
 setx GPU_SINGLE_ALLOC_PERCENT   100
-ethminer --farm-recheck 200 -G -S us1.ethermine.org:4444 -FS eu1.ethermine.org:4444 -O 0xDd9d279854C8D614d37a492192A40f678f476BC1.$(hostname)
+ethminer --farm-recheck 200 -G -S $SERVER1 -FS $SERVER2 -O $ETHER_ADDR.$(hostname)
