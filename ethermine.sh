7e04136133ee5b0c065867eb7edd5325f17b9774
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-09-02 01:32:00 -0500

    Use CUDA; have performance setting

--- ethermine.sh
+++ ethermine.sh
@@ -3,9 +3,10 @@ ETHER_ADDR=0xDd9d279854C8D614d37a492192A40f678f476BC1
 SERVER1=us1.ethermine.org:4444
 SERVER2=us2.ethermine.org:4444
 
-setx GPU_FORCE_64BIT_PTR        0
-setx GPU_MAX_HEAP_SIZE          100
-setx GPU_USE_SYNC_OBJECTS       1
-setx GPU_MAX_ALLOC_PERCENT      100
-setx GPU_SINGLE_ALLOC_PERCENT   100
-ethminer --farm-recheck 200 -G -S $SERVER1 -FS $SERVER2 -O $ETHER_ADDR.$(hostname)
+CUDA_BLOCK_SIZE_DEF=$((128))
+CUDA_GRID_SIZE_DEF=$((8192))
+
+CUDA_BLOCK_SIZE=$((CUDA_BLOCK_SIZE_DEF / 1))
+CUDA_GRID_SIZE=$((CUDA_GRID_SIZE_DEF / 1))
+ethminer    --farm-recheck 200 -RH -G -S $SERVER1 -FS $SERVER2 -O $ETHER_ADDR.$(hostname)\
+            --cuda-block-size $CUDA_BLOCK_SIZE --cuda-grid-size $CUDA_GRID_SIZE
